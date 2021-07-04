9244da4855e3ec25058590fc8ce01b5f0dde9773
Krypt0mux <33673319+ethicalhackingplayground@users.noreply.github.com>
2021-07-04 22:17:09 +1000

    Add files via upload

--- pdtools.sh
+++ pdtools.sh
@@ -1,15 +1,39 @@
 #!/bin/bash
 
 
+function banner() {
+
+echo "            _ _              _     	"
+echo "           | | |            | |    	"
+echo "  _ __   __| | |_ ___   ___ | |___ 	"	
+echo " | '_ \ / _  | __/ _ \ / _ \| / __|	"
+echo " | |_) | (_| | || (_) | (_) | \__ \	"
+echo " | .__/ \__,_|\__\___/ \___/|_|___/	"
+echo " | |                               	"
+echo " |_|                               	"
+echo " 		Updater/Installer		"
+echo ""
+echo ""
+
+}
+
 # Install golang
-function golang() {
+function main() {
+
+	# Display the ASCII banner
+	banner
 	
-	if [ "$1" != "golang" ]; then
-		operator
+	if [ "$1" != "go" ]; then
+
+		# Download pd tools
+		echo "----- [+] Installing pd tools"
+		echo ""
+		execute_tools
 	else
-
-
-		# Download golang language
+		
+		# Download golang language and install pd tools
+		echo "----- [+] Installing Golang and pd tools"
+		echo ""
 		wget https://dl.google.com/go/go1.16.5.linux-amd64.tar.gz
 		sudo tar -xvf go1.16.5.linux-amd64.tar.gz
 		sudo mv go /usr/local
@@ -20,12 +44,12 @@ function golang() {
 		echo 'export GOPATH=$HOME/go'	>> ~/.bash_profile			
 		echo 'export PATH=$GOPATH/bin:$GOROOT/bin:$PATH' >> ~/.bash_profile	
 		rm *.gz
-		operator
+		execute_tools
 	fi
 }
 
 # Update/Install the project discovery tools tools
-function operator() {
+function execute_tools() {
 	GO111MODULE=on go get -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei
 	GO111MODULE=on go get -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder
 	GO111MODULE=on go get -v github.com/projectdiscovery/httpx/cmd/httpx
@@ -35,5 +59,4 @@ function operator() {
 }
 
 # Run functions sequentially.
-golang
-operator
+main $1
